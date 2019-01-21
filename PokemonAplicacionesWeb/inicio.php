c675f44e16673e98c53effed87ed0fc1b25bb09b
lukaxa888 <43339503+lukaxa888@users.noreply.github.com>
2019-01-21 13:36:56 +0100

    eeeeee

--- PokemonAplicacionesWeb/inicio.php
+++ PokemonAplicacionesWeb/inicio.php
@@ -8,20 +8,14 @@ if (!isset($_SESSION['user'])) {
 
 <html>
   <head>
-    <link rel="stylesheet" type="text/css" href="inicio.css">
     <title>Pokemon</title>
-
   </head>
   <body background="fondo.png">
-
-<span><span></span></span>
-<div class="wrap">
-  <a href="pokedex.php"><div></div></a>
-  <a href="regiones.php"><div></div></a>
-  <a href="tipos.php"><div></div></a>
-  <a href="#"><div></div></a>
-  <a href="#"><div></div></a>
-</div>
-
+    <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+    <center>
+    <form action="../cerrarsesion.php" method="get">
+      <button >Cerrar Sesion</button>
+    </form>
+</center>
   </body>
 </html>
