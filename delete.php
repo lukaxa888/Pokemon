be95cea49c6d08e30fc5ed410a2911cad4f3469e
julenm22 <43339312+julenm22@users.noreply.github.com>
2019-01-31 13:06:56 +0100

    Update delete.php

--- delete.php
+++ delete.php
@@ -19,7 +19,7 @@ $IdPokemon = $_POST["IdPokemon"];
 
 //$IdUsuario = "SELECT Id from Usuarios Where Nombre = $usuario;"
 // INSERT INTO pokeusuarios (IdUsuario, IdPokemon) VALUES ($IdUsuario, $IdPokemon) 
-$query = "DELETE FROM pokeusuarios (IdUsuario, IdPokemon) WHERE IdUsuario = $IdUsuario AND IdPokemon = $IdPokemon);";
+$query = "DELETE FROM pokeusuarios WHERE IdUsuario = $IdUsuario AND IdPokemon = $IdPokemon;";
 
 //$result = mysqli_query($conn, $query);
 
